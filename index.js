4bed1bc9172055ad8bc1c25cbd177e030aa92030
nagraj0308 <nagendrachaudhary2016@gmail.com>
2022-09-28 16:38:13 +0530

    dh

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-
-
-const URL = "https://messnitrklserver.herokuapp.com/api/hostel/";
+const URL = "http://13.234.156.148:6000/api/hostel/";
 function showpb() {
   var x = document.getElementById("loaderouter");
-
   x.style.display = "block";
-
 }
 function hidepb() {
   var x = document.getElementById("loaderouter");
@@ -13,42 +9,39 @@ function hidepb() {
   x.style.display = "none";
 }
 
-
 function getMessData(hostel, meal) {
   showpb();
   fetch(URL, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      "Content-type": "application/json; charset=UTF-8"
+      "Content-type": "application/json; charset=UTF-8",
     },
 
     body: JSON.stringify({
-      "hostel": hostel,
-      "meal": meal
-    })
+      hostel: hostel,
+      meal: meal,
+    }),
   })
-    .then(response => response.json())
-    .then(data => {
+    .then((response) => response.json())
+    .then((data) => {
       hidepb();
       var veg = parseInt(data.data.veg);
       var nonveg = parseInt(data.data.nonveg);
       var total = veg + nonveg;
-      document.querySelector('#not_eating').innerHTML = data.data.absent;
-      document.querySelector('#veg_eating').innerHTML = veg + '/' + total;
-      document.querySelector('#non_veg_eating').innerHTML = nonveg + '/' + total;
+      document.querySelector("#not_eating").innerHTML = data.data.absent;
+      document.querySelector("#veg_eating").innerHTML = veg + "/" + total;
+      document.querySelector("#non_veg_eating").innerHTML =
+        nonveg + "/" + total;
     })
     // {"isTrue":1,"msg":"","data":{"absent":0,"veg":0,"nonveg":0}}
-    .catch(error => {
+    .catch((error) => {
       hidepb();
       window.alert("Error in fetching error!!");
     });
-
 }
 
 function submitData() {
-  var hostel_name = document.querySelector('#hostel_name').value;
-  var meal_code = document.querySelector('#meal_code').value;
+  var hostel_name = document.querySelector("#hostel_name").value;
+  var meal_code = document.querySelector("#meal_code").value;
   getMessData(hostel_name, meal_code);
-
 }
-
